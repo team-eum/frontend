8e179f4b04d64da8a70b5ec36f699e9f729b91c5
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-01-19 03:35:15 +0900

    feat: 랜딩페이지 로그인 후 퍼블리싱

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,6 +1,7 @@
 import Button from "../../components/button/Button.jsx";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/header/Header.jsx";
+import btn from "../../assets/Landing_Icon.png";
 
 import {
   Container,
@@ -8,10 +9,16 @@ import {
   SubTitleText,
   TitleText,
   SubText,
+  MainButton,
+  ButtonContainer,
+  BtnImage,
+  BtnText,
 } from "./styles.jsx";
+import { useUser } from "../../domain/UserContext.jsx";
 
 function LandingPage() {
   const navigate = useNavigate();
+  const { user } = useUser();
 
   return (
     <Container>
@@ -23,11 +30,28 @@ function LandingPage() {
           모두를 이어 함께 성장하는 플랫폼 EUM에서 더 나은 미래를 만들어 가세요.
         </SubText>
       </TextContainer>
-      <Button
-        className="skyblue"
-        title="EUM 사용하러 가기"
-        onClick={() => navigate("/login")}
-      />
+      {user ? (
+        <Button
+          className="skyblue"
+          title="EUM 사용하러 가기"
+          onClick={() => navigate("/login")}
+        />
+      ) : (
+        <ButtonContainer>
+          <MainButton className="blue" onClick={() => navigate("/matching")}>
+            <BtnImage image={btn}></BtnImage>
+            <BtnText>매칭 목록 확인하기</BtnText>
+          </MainButton>
+          <MainButton className="white" onClick={() => navigate("/schedule")}>
+            <BtnImage image={btn}></BtnImage>
+            <BtnText>일정 목록 확인하기</BtnText>
+          </MainButton>
+          <MainButton className="blue" onClick={() => navigate("/interview")}>
+            <BtnImage image={btn}></BtnImage>
+            <BtnText>면담 내역 확인하기</BtnText>
+          </MainButton>
+        </ButtonContainer>
+      )}
     </Container>
   );
 }
--- src/pages/landing/styles.jsx
+++ src/pages/landing/styles.jsx
@@ -47,3 +47,44 @@ export const SubText = styled.h3`
   font-size: 24px;
   font-weight: 400;
 `;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+  padding: 20px 0;
+`;
+
+export const MainButton = styled.div`
+  width: 250px;
+  height: 250px;
+  border: 1px solid #000000;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  &.blue {
+    background: #c2e0f7;
+  }
+
+  &.white {
+    background: #f5f5f5;
+  }
+`;
+
+export const BtnImage = styled.div`
+  width: 128px;
+  height: 128px;
+  margin-top: 20px;
+  background-image: url(${(props) => props.image});
+  background-size: cover;
+  background-position: center;
+`;
+
+export const BtnText = styled.div`
+  font-weight: 700;
+  font-size: 26px;
+  text-align: center;
+  color: #000000;
+`;
