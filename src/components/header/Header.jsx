df03a45bddfe18f480246e2cbb486e48992a8fae
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-01-19 03:16:10 +0900

    feat: 로그인 후 헤더 퍼블리싱

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,27 +2,35 @@ import Logo from "../../assets/Logo.png";
 import Button from "../button/Button";
 import { useNavigate } from "react-router-dom";
 import { ButtonContainer, Container, LogoImage } from "./styles";
+import { useUser } from "../../domain/UserContext";
+import { FiUser } from "react-icons/fi";
 
 function Header() {
-  //const { user } = useUser();
+  const { user } = useUser();
   const navigate = useNavigate();
 
-  //todo : 로그인 후 header 구현
   return (
     <Container>
-      <LogoImage image={Logo}></LogoImage>
-      <ButtonContainer>
-        <Button
-          className="skyblue"
-          title="로그인"
-          onClick={() => navigate("/login")}
-        />
-        <Button
-          className="grey"
-          title="회원가입"
-          onClick={() => navigate("/login")}
-        />
-      </ButtonContainer>
+      <LogoImage image={Logo} onClick={() => navigate("/")}></LogoImage>
+      {user ? (
+        <ButtonContainer className="icon">
+          <FiUser
+            size="30"
+            onClick={() => {
+              navigate("/mypage");
+            }}
+          />
+        </ButtonContainer>
+      ) : (
+        <ButtonContainer>
+          <NavItem to="/login">
+            <Button title="로그인" className="yellow"></Button>
+          </NavItem>
+          <NavItem to="/register">
+            <Button title="회원가입" className="brown"></Button>
+          </NavItem>
+        </ButtonContainer>
+      )}
     </Container>
   );
 }
