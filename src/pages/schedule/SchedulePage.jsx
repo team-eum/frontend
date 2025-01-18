6cf426b81051f104e5247108791318c84bb5c728
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-01-19 01:59:47 +0900

    feat: 스케쥴페이지 퍼블리싱

--- src/pages/schedule/SchedulePage.jsx
+++ src/pages/schedule/SchedulePage.jsx
@@ -1,22 +1,22 @@
 //import { useNavigate } from "react-router-dom";
 import Header from "../../components/header/Header.jsx";
-import ListItem from "../../components/list/MatchingItem.jsx";
+import ListItem from "../../components/list/ScheduleItem.jsx";
 import { Container, TextContainer, TitleText, SubText } from "./styles.jsx";
 
-function MatchingPage() {
+function SchedulePage() {
   //const navigate = useNavigate();
 
   return (
     <Container>
       <Header />
       <TextContainer>
-        <TitleText>나의 매칭 목록</TitleText>
-        <SubText>나에게 매칭된 멘토/멘티를 확인하세요!</SubText>
+        <TitleText>나의 일정 목록</TitleText>
+        <SubText>확정된 일정을 확인하세요!</SubText>
       </TextContainer>
       {/* todo: api 연동 후 list 매핑 */}
-      <ListItem name="홍길동" keyword="키오스크 확인" date="월 14:00 -16:00" />
+      <ListItem name="홍길동" keyword="키오스크 확인" date="월 14:00 - 16:00" />
     </Container>
   );
 }
 
-export default MatchingPage;
+export default SchedulePage;
